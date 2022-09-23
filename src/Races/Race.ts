f4993f64751d525b8c11b972ff6c7b8e12e3899d
matheusnff85 <math.marinho154@gmail.com>
2022-09-23 14:41:02 +0000

    feat: implementa requisito 1 classe Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,23 +1,20 @@
-abstract class Race {
-  private _name: string;
-  private _dexterity: number;
+export default abstract class Race {
+constructor(
+  private _name: string, 
+  private _dexterity: number
+  ) { }
 
-  constructor(name: string, dexterity: number) {
-    this._name = name;
-    this._dexterity = dexterity;
+  public get name(): string {
+    return this._name;
   }
 
-  public static createdRacesInstances(): number {
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
 
   abstract get maxLifePoints(): number;
-
-  get name(): string {
-    return this._name;
-  }
-
-  get dexterity(): number {
-    return this._dexterity;
-  }
 }
