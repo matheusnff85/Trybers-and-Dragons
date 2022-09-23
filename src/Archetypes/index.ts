02337279a599dbba1bbfa010469323a249b956cf
matheusnff85 <math.marinho154@gmail.com>
2022-09-23 12:54:49 -0300

    feat: implementa subclasses de archetype requisito 5

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,9 @@
 import Archetype from './Archetype';
+import Ranger from './Ranger';
+import Warrior from './Warrior';
+import Mage from './Mage';
+import Necromancer from './Necromancer';
 
 export default Archetype;
+
+export { Ranger, Warrior, Mage, Necromancer };
